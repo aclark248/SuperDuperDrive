d3b3d4d503a00b06463791c43d7a8fce33ceec02
andrewaclark248 <55842531+andrewaclark248@users.noreply.github.com>
2021-07-24 14:06:45 -0400

    Basic auth changes

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -0,0 +1,34 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+@Service
+public class AuthenticationService implements AuthenticationProvider {
+    private UserMapper userMapper;
+    private HashService hashService;
+
+    public AuthenticationService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userMapper.getUser(username);
+        if (user != null) {
+            String encodedSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+            if (user.getPassword().equals(hashedPassword)) {
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
